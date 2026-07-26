5a720f387c8693fa368e9ec7128e5488af435f21
LSickle <davidboy69@hotmail.com>
2026-07-26 00:18:59 -0500

    samesite none5

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Config;
+
+Route::get('/debug-env', function () {
+    return response()->json([
+        'SESSION_DRIVER' => config('session.driver'),
+        'SESSION_SAME_SITE' => config('session.same_site'),
+        'SESSION_SECURE_COOKIE' => config('session.secure'),
+        'SANCTUM_STATEFUL_DOMAINS' => config('sanctum.stateful'),
+        'FRONTEND_URL' => config('cors.allowed_origins'),
+        'APP_ENV' => config('app.env'),
+    ]);
+});
